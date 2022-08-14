991c63f51ff5a4a785f9646441a9d5141ca03141
TEMPIK-XD <107808648+TEMPIK-DEV@users.noreply.github.com>
2022-08-14 21:07:44 +0700

    Update run.py

--- run.py
+++ run.py
@@ -63,7 +63,7 @@ U='\033[1;35m' #UNGU
 O='\033[38;2;255;127;0;1m' #ORANGE
 C='\033[0m' #CLEAR
 N = '\x1b[0m' # WARNA MATI
-USN="MozillaMozilla/5.0 (Linux; Android 6.0.1; ASUS_Z010D Build/MMB29P; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/46.0.2490.76 Mobile Safari/537.36 Instagram 41.0.0.13.92 Android (23/6.0.1; 320dpi; 720x1280; asus; ASUS_Z010D; ASUS_Z010; qcom; ru_RU; 103516660)"
+USN="Mozilla/5.0 (Linux; Android 7.0; MEIZU M6 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/66.0.3359.106 Mobile Safari/537.36 Instagram 41.0.0.13.92 Android (24/7.0; 320dpi; 720x1196; Meizu; MEIZU M6; meizu_M6; mt6755; ru_RU; 103516660)"
 # USN="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
 ugen=open('ua.txt','r').read().splitlines()
 internal,external,success,checkpoint,loop,following,lisensikuni,lisensiku=[],[],[],[],0,[],[],['sukses']
@@ -696,7 +696,7 @@ def lisensi():
     except:
         tlisensi()
     ses=requests.Session()
-    res=ses.get('https://app.cryptolens.io/api/key/Activate?token=WyIyMTg4MDYzMCIsIi9lUjRxSjZXZ0swZzJrZ2NlaHVqMkFCajh2UC9qQjJlV1FXY3lUVjMiXQ==&ProductId=15855&Key='+lisensikuni[0]).json()
+    res=ses.get('https://app.cryptolens.io/api/key/Activate?token=WyIyMzU4NjUxMCIsIlRMSVY3Mk52YXQ1Ym16WG1PUy9jelNVbEx1UmppTWlLSDkzaTFWM1IiXQ==&ProductId=16355&Key='+lisensikuni[0]).json()
     status=res['licenseKey']['key']
     if status ==cek:
         banner()
@@ -773,4 +773,3 @@ if __name__=='__main__':
         lisensi()
     except requests.exceptions.ConnectionError:
         exit(f'\n [{M}!{C}] Koneksi internet bermasalah')
-
